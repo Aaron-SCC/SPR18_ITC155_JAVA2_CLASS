8c64498f52abec5c00885e432ed95b093b25ea97
Aaron-SCC <32554879+Aaron-SCC@users.noreply.github.com>
2018-05-15 15:41:38 -0700

    Add files via upload

--- 2018_05_15_MidTerm/MidTerm_Probelm02.java
+++ 2018_05_15_MidTerm/MidTerm_Probelm02.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
 
 public class MidTerm_Probelm02 {
 
@@ -7,6 +11,22 @@ public class MidTerm_Probelm02 {
 		System.out.println("THIS IS A TEMPLATE MidTerm Problem #02 \n\n");
 		
 		Scanner console = new Scanner(System.in);
+		
+		System.out.println(mystery3(6));
+		System.out.println(mystery3(17));
 	}
+	
+	
+	public static int mystery3(int n) {
+		if (n < 0) {
+			return -mystery3(-n);
+		} else if (n < 10) {
+			return n;
+		} else {
+			return mystery3(n / 10 + n % 10);
+		}
+		
+	}
+	
 
 }
