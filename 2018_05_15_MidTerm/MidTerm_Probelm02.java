4b6189ce6d19dad5c878aedd4585f302ab84c967
Aaron-SCC <32554879+Aaron-SCC@users.noreply.github.com>
2018-05-15 15:19:07 -0700

    Add files via upload

---
+++ 2018_05_15_MidTerm/MidTerm_Probelm02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MidTerm_Probelm02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("THIS IS A TEMPLATE \n\n");
+		
+		Scanner console = new Scanner(System.in);
+	}
+
+}
